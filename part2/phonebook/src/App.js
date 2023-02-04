9b05b4f510d7b31226ef99fcdc1c70c9b692b6dc
emmaw <emma.wishart@live.com.au>
2023-02-04 16:17:36 +1000

    exercise 2.9: Phonebook step4 - search name and return full object

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,11 +2,23 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: 990}
+    {
+      name: 'Arto Hellas',
+      number: 990
+    },
+    {
+      name: 'Amy Lala',
+      number: 889
+    },
+    {
+      name: 'Rand tom',
+      number: 889
+    }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setSearch] = useState('')
+  const [showFiltered, setShowFiltered] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -16,27 +28,50 @@ const App = () => {
         alert(`"${newName}" has already been added to the phonebook`)
       }
       else {
-        const personObject = { 
+        const personObject = {
           name: newName,
           number: newNumber
         }
         setPersons(persons.concat(personObject));
       }
-    }
-    )
-
+    })
   }
+
+  // const findMatch = (searchArray, searchString) => searchArray.filter(element => {
+  //   if (element.toLowerCase().includes(searchString.toLowerCase()))
+  //     return true;
+  // })
+
+  function findByMatchingProperties(objectToSearch, keyValueToMatch) {
+    return objectToSearch.filter(function (objectEntry) {
+        return Object.keys(keyValueToMatch).every(function (key) {
+          if (objectEntry[key].toLowerCase().includes(keyValueToMatch[key].toLowerCase()))
+          return true;
+        });
+    });
+}
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+    console.log(event.target.value)
   }
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  const handSearchChange = (event) => {
+    setSearch(event.target.value)
+    setShowFiltered(findByMatchingProperties(persons, { name: event.target.value}))
+  }
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        Filter shown with: <input
+          value={newSearch}
+          onChange={handSearchChange} />
+      </div>
+      <h2>Add New</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input
@@ -44,18 +79,29 @@ const App = () => {
             onChange={handleNameChange} />
         </div>
         <div>number: <input
-            value={newNumber}
-            onChange={handleNumberChange} /></div>
+          value={newNumber}
+          onChange={handleNumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => {
-          return (<li key={person.name}>{person.name}: {person.number}</li>)
-        })}
-      </ul>
+      <div>
+        {
+          newSearch === '' ?
+            <ul>
+              {persons.map((person) => {
+                return (<li key={person.name}>{person.name}: {person.number}</li>)
+              })}
+            </ul>
+            :
+            <ul>
+              {showFiltered.map((person) => {
+                return (<li key={person.name}>{person.name}: {person.number}</li>)
+              })}
+            </ul>
+        }
+      </div>
     </div>
   )
 }
